1341c5b4d4aa01ab29f9323c8256a08aa351bedc
cereninfinia <ceren.eycan@infinia.com.tr>
2024-01-19 16:25:03 +0300

    asdfsdfds

--- Assets/GridPlacer.cs
+++ Assets/GridPlacer.cs
@@ -1,34 +1,79 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GridPlacer : MonoBehaviour
 {
-    public GameObject[] gameObjects; // Oyun objeleri için dizi
-    public float spacing = 2.0f;     // Objeler arasý boþluk
+    public Button[] buttons; // Oyun objeleri için dizi
+    public float spacing = 80f; // Objeler arasý boþluk
+
+    private bool[] buttonStates; // Butonlarýn basýlý durumunu takip etmek için
 
     void Start()
     {
-        
-        if (gameObjects.Length != 9)
+        InitializeButtons();
+    }
+
+    void InitializeButtons()
+    {
+        if (buttons.Length != 9)
         {
             Debug.LogError("9 oyun objesi gerekiyor.");
             return;
         }
 
+        buttonStates = new bool[buttons.Length];
+
+        float centerX = Screen.width / 2f; // Ekranýn x ortasý
+        float centerY = Screen.height / 2f; // Ekranýn y ortasý
+
         int index = 0;
-        for (int y = 0; y < 3; y++)
+        for (int y = -1; y <= 1; y++)
         {
-            for (int x = 0; x < 3; x++)
+            for (int x = -1; x <= 1; x++)
             {
-                // Objeyi belirli bir pozisyona yerleþtir
-                GameObject obj = gameObjects[index];
-                float posX = x * spacing;
-                float posY = y * -spacing; // Unity'de y ekseninin yukarý doðru pozitif olduðunu unutmayýn
+                Button button = buttons[index];
+                GameObject obj = button.gameObject;
+
+                // Butonlarý ekranýn ortasýna yerleþtir
+                float posX = centerX + x * spacing;
+                float posY = centerY - y * spacing;
+
                 obj.transform.position = new Vector3(posX, posY, 0);
+
+                // Butona basýldýðýnda çalýþacak metodu atayýn
+                int buttonIndex = index; // Her buton için özel bir index oluþtur
+                AddEventTrigger(button.gameObject, buttonIndex);
 
                 index++;
             }
         }
     }
-    
+
+    void AddEventTrigger(GameObject buttonObj, int buttonIndex)
+    {
+        EventTrigger trigger = buttonObj.GetComponent<EventTrigger>() ?? buttonObj.AddComponent<EventTrigger>();
+
+        var pointerDown = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
+        pointerDown.callback.AddListener((data) => { OnButtonDown(buttonIndex); });
+        trigger.triggers.Add(pointerDown);
+
+        var pointerUp = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
+        pointerUp.callback.AddListener((data) => { OnButtonUp(buttonIndex); });
+        trigger.triggers.Add(pointerUp);
+    }
+
+    void OnButtonDown(int buttonIndex)
+    {
+        buttonStates[buttonIndex] = true;
+        Debug.Log("Button " + buttonIndex + " pressed");
+    }
+
+    void OnButtonUp(int buttonIndex)
+    {
+        buttonStates[buttonIndex] = false;
+        Debug.Log("Button " + buttonIndex + " released");
+    }
 }
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,20 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GridController : MonoBehaviour
+public class GridController : MonoBehaviour, IGrid
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Image gridImage; // Grid butonunun görselini kontrol etmek için kullanýlýr
 
+    private Color lightUpColor = Color.white; // Açýk renk
+    private Color lightDownColor = Color.gray; // Kapalý renk
+    private Color lightSuccessColor = Color.green; // Baþarý rengi
+    private Color lightFailColor = Color.red; // Baþarýsýzlýk rengi
+    private Color lightInitialColor = Color.blue; // Ýlk renk
+    private void Start()
+    {
+        gridImage = GetComponent<Image>();
+    }
+    public void LightUp()
+    {
+        gridImage.color = lightUpColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LightDown()
     {
-
+        gridImage.color = lightDownColor;
     }
 
+    public void LightSuccess()
+    {
+        gridImage.color = lightSuccessColor;
+    }
 
+    public void LightFail()
+    {
+        gridImage.color = lightFailColor;
+    }
+
+    public void LightInitial()
+    {
+        gridImage.color = lightInitialColor;
+    }
+
+    public void Init()
+    {
+        // Burada baþlangýç ayarlarýný yapabilirsiniz
+        LightInitial();
+    }
+
+    public void Pressed()
+    {
+        // Burada butona basýldýðýnda yapýlacak iþlemleri ekleyebilirsiniz
+        LightUp();
+    }
 }
